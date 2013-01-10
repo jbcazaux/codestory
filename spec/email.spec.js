25fe712d0569de46db9106e52a6cc08aa4dcbba6
jbcazaux <jbcazaux@gmail.com>
2013-01-10 15:56:37 +0100

    switch to jasmine for testing

---
+++ spec/email.spec.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+var myserver = "http://localhost:8123";
+
+describe("A suite", function() {
+	it("should respond with hello world", function(done) {
+		request(myserver + "/?q=Quelle+est+ton+adresse+email", function(error, response, body){
+			expect(body).toEqual("jbcazaux@gmail.com");
+			done();
+		});
+	});
+});
+
+	
