d63d783c49db43edbe60120e9e8011b81db1e2fd
jbcazaux <jbcazaux@gmail.com>
2013-01-10 16:06:12 +0100

    clean: bad test description

--- spec/email.spec.js
+++ spec/email.spec.js
@@ -3,7 +3,7 @@ var request = require('request');
 var myserver = "http://localhost:8123";
 
 describe("A suite", function() {
-	it("should respond with hello world", function(done) {
+	it("should respond with my email", function(done) {
 		request(myserver + "/?q=Quelle+est+ton+adresse+email", function(error, response, body){
 			expect(body).toEqual("jbcazaux@gmail.com");
 			done();
