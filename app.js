428ce63a46498c77c61dc370d913b8a3cc777603
jbcazaux <jbcazaux@gmail.com>
2013-01-13 16:52:17 +0100

    oops: mispelling qix

--- app.js
+++ app.js
@@ -38,19 +38,23 @@ app.get('/scalaskel/change/:X', function(req, res) {
 	res.json(getChanges(req.params.X));
 });
 
+var _BAZ = 21;
+var _QIX = 11;
+var _BAR = 7;
+var _FOO = 1;
+
 function getChanges(change){
 	var possibilities = [];
-	for (var baz = 0; 21 * baz <= change; baz++){
-		var changebaz = change - 21*baz;
-		for (var quix = 0; 11 * quix <= changebaz; quix++){
-			var changequix = changebaz - 11*quix;
-			
-			for (var bar = 0; 7 * bar <= changequix; bar++){
-				var changebar = changequix - 7*bar;
+	for (var baz = 0; _BAZ * baz <= change; baz++){
+		var changebaz = change - _BAZ*baz;
+		for (var qix = 0; _QIX * qix <= changebaz; qix++){
+			var changeqix = changebaz - _QIX*qix;
+			for (var bar = 0; _BAR * bar <= changeqix; bar++){
+				var changebar = changeqix - _BAR*bar;
 				var possibility = {};
 				if (changebar > 0) possibility['foo'] = changebar;
 				if (bar > 0) possibility['bar'] = bar;
-	  			if (quix > 0) possibility['quix'] = quix;
+	  			if (qix > 0) possibility['qix'] = qix;
 				if (baz > 0) possibility['baz'] = baz;
 				possibilities.push(possibility);
 			}
--- spec/change.spec.js
+++ spec/change.spec.js
@@ -57,11 +57,24 @@ describe("phase 1 - change", function() {
 			done();
 		});
 	});
+	it("should answser correctly to change 11", function(done) {
+		request(myserver + "/scalaskel/change/11", function(error, response, body){
+			var answer = JSON.parse(body);
+			expect(answer.length).toBe(3);			
+			var expected = [{'foo':11}, {'bar':1, 'foo':4}, {'qix':1}];
+			for (var comb in answer){
+				for (var piece in answer[comb]){
+					expect(answer[comb][piece]).toBe(expected[comb][piece]);
+				}
+			}
+			done();
+		});
+	});
 	it("should answser correctly to change 15", function(done) {
 		request(myserver + "/scalaskel/change/15", function(error, response, body){
 			var answer = JSON.parse(body);
 			expect(answer.length).toBe(4);			
-			var expected = [{'foo':15}, {'bar':1,'foo':8}, {'bar':2, 'foo':1}, {'quix':1, 'foo':4}];
+			var expected = [{'foo':15}, {'bar':1,'foo':8}, {'bar':2, 'foo':1}, {'qix':1, 'foo':4}];
 			for (var comb in answer){
 				for (var piece in answer[comb]){
 					expect(answer[comb][piece]).toBe(expected[comb][piece]);
@@ -74,7 +87,7 @@ describe("phase 1 - change", function() {
 		request(myserver + "/scalaskel/change/22", function(error, response, body){
 			var answer = JSON.parse(body);
 			expect(answer.length).toBe(8);			
-			var expected = [{'foo':22}, {'bar':1,'foo':15}, {'bar':2, 'foo':8}, {'bar':3, 'foo':1}, {'quix':1, 'foo':11}, {'quix':1, 'bar': 1, 'foo':4}, {'quix':2}, {'baz': 1, 'foo': 1}];
+			var expected = [{'foo':22}, {'bar':1,'foo':15}, {'bar':2, 'foo':8}, {'bar':3, 'foo':1}, {'qix':1, 'foo':11}, {'qix':1, 'bar': 1, 'foo':4}, {'qix':2}, {'baz': 1, 'foo': 1}];
 			for (var comb in answer){
 				for (var piece in answer[comb]){
 					expect(answer[comb][piece]).toBe(expected[comb][piece]);
