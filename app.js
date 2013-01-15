706bce568e955a92d0a3e357c6c8350c92b16630
jbcazaux <jbcazaux@gmail.com>
2013-01-15 12:05:11 +0100

    change evaluation algorythm, eval() is not used anymore

--- app.js
+++ app.js
@@ -22,10 +22,11 @@ app.get('/', function(req, res){
 
 	var rawq = req._parsedUrl.query.split('=')[1];
 	if (rawq) {
-		var calc=/^[\d\-\+/\*\)\(]+$/gi;
+		var calc=/^[\d\-\+/\*\)\(,]+$/gi;
 		if (calc.test(rawq)){
 			console.log(rawq + " = " + eval(rawq));
-			res.send(200, eval(rawq).toString());
+			//res.send(200, eval(rawq.replace(',', '.')).replace('.', ","));
+			res.send(evaluateExpression(infixToPostfix(rawq.replace(',', '.'))).toString().replace('.', ","));			
 			return;
 		}
 	}
@@ -51,4 +52,140 @@ app.get('/scalaskel/change/:X', function(req, res) {
 	res.json(scalaskel.getChanges(req.params.X));
 });
 
+function notEmpty(val){
+	return val && val != '';
+}
 
+Object.defineProperty(Array.prototype, 'last', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+        return this.length > 0 ? this[this.length - 1] : undefined;
+    },
+    set: undefined
+});
+
+	
+function evaluateExpression(expression)
+{
+    var tokens = expression.split(/([\d\.]+|[*\+\-/\(\)])/).filter(notEmpty);
+    var evalStack = [];
+
+    while (tokens.length != 0)
+    {
+        var currentToken = tokens.shift();
+
+        if (isNumber(currentToken))
+        {
+            evalStack.push(currentToken);
+        }
+        else if (isOperator(currentToken))
+        {
+            var operand2 = evalStack.pop();
+            var operand1 = evalStack.pop();
+
+            var result = performOperation(operand1, operand2, currentToken);
+            evalStack.push(result);
+        }
+    }
+    return evalStack.pop();
+}
+
+function performOperation(operand1, operand2, operator)
+{
+    switch(operator)
+    {
+        case '+': 
+            return operand1 + operand2;
+        case '-':
+            return operand1 - operand2;
+        case '*':
+            return operand1 * operand2;
+        case '/':
+            return operand1 / operand2;
+        default:
+            return;
+    }
+
+}
+
+
+function infixToPostfix(expression)
+{
+    var tokens = expression.split(/([\d\.]+|[\*\+\-\/\(\)])/).filter(notEmpty);
+    var outputQueue = [];
+    var operatorStack = [];
+
+    while (tokens.length > 0)
+    {
+        var currentToken = tokens.shift();
+
+        if (isNumber(currentToken)) 
+        {
+            outputQueue.push(currentToken);
+        }
+        else if (isOperator(currentToken)) 
+        {
+            while (getPrecedence(currentToken) <= getPrecedence(operatorStack.last) ) 
+            {
+                outputQueue.push(operatorStack.pop())
+            }
+
+            operatorStack.push(currentToken);
+
+        }
+        else if (currentToken == '(')
+        {
+                operatorStack.push(currentToken);
+        }
+        else if (currentToken == ')')
+        {
+            while (operatorStack.last != '(')
+            {
+    	        if (operatorStack.length == 0)
+    	            return "Error in braces count";
+
+    	        outputQueue.push(operatorStack.pop());
+            }	
+            operatorStack.pop();		
+        }   
+    }  
+
+    while (operatorStack.length != 0)
+    {
+        if (/^[\(\)]$/.test(operatorStack.last))
+		return "Error in braces count";
+        
+	outputQueue.push(operatorStack.pop());
+                     
+    }
+
+    return outputQueue.join(" ");
+}    
+
+
+function isOperator(token)
+{
+    return /^[*\+\-\/]$/.test(token);
+}
+
+function isNumber(token)
+{
+    return /^\d*(\.\d+)?$/.test(token);
+}
+
+
+function getPrecedence(token)
+{
+    switch (token)
+    {
+    	case '*':		    
+    	case '/':
+    	    return 8;
+        case '+':
+    	case '-':
+    	    return 6;
+    	default: 
+    	    return -1;
+    }
+}
--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -10,4 +10,28 @@ describe("phase 1 - change", function() {
 			done();
 		});
 	});
+	it("should answser correctly to 9*9", function(done) {
+		request(myserver + "/?q=9*9", function(error, response, body){
+			expect(body).toBe('81');
+			done();
+		});
+	});
+	it("should answser correctly to (8+4)/2", function(done) {
+		request(myserver + "/?q=(8+4)/2", function(error, response, body){
+			expect(body).toBe('6');
+			done();
+		});
+	});
+	it("should answser correctly to (1+2)/2", function(done) {
+		request(myserver + "/?q=(1+2)/2", function(error, response, body){
+			expect(body).toBe('1,5');
+			done();
+		});
+	});
+	it("should answser correctly to 1,5*4", function(done) {
+		request(myserver + "/?q=1,5*4", function(error, response, body){
+			expect(body).toBe('6');
+			done();
+		});
+	});
 });
